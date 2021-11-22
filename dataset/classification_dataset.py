5cfd6f655e0dbdb52270eb36e56b184304f97d6c
AinL <gmlwns5176@gmail.com>
2021-11-22 21:11:17 +0900

    add labels

--- dataset/classification_dataset.py
+++ dataset/classification_dataset.py
@@ -5,11 +5,61 @@ import transformers, torch, tqdm
 
 from dataset.classification_batch_entry import ClassificationBatchEntry
 
+LABELS = {
+    'AG_NEWS': {0: 'World',
+                1: 'Sports',
+                2: 'Business',
+                3: 'Sci/Tech'},
+    'SogouNews': {0: 'Sports',
+                  1: 'Finance',
+                  2: 'Entertainment',
+                  3: 'Automobile',
+                  4: 'Technology'},
+    'DBpedia': {0: 'Company',
+                1: 'EducationalInstitution',
+                2: 'Artist',
+                3: 'Athlete',
+                4: 'OfficeHolder',
+                5: 'MeanOfTransportation',
+                6: 'Building',
+                7: 'NaturalPlace',
+                8: 'Village',
+                9: 'Animal',
+                10: 'Plant',
+                11: 'Album',
+                12: 'Film',
+                13: 'WrittenWork'},
+    'YelpReviewPolarity': {0: 'Negative polarity',
+                           1: 'Positive polarity'},
+    'YelpReviewFull': {0: 'score 1',
+                       1: 'score 2',
+                       2: 'score 3',
+                       3: 'score 4',
+                       4: 'score 5'},
+    'YahooAnswers': {0: 'Society & Culture',
+                     1: 'Science & Mathematics',
+                     2: 'Health',
+                     3: 'Education & Reference',
+                     4: 'Computers & Internet',
+                     5: 'Sports',
+                     6: 'Business & Finance',
+                     7: 'Entertainment & Music',
+                     8: 'Family & Relationships',
+                     9: 'Politics & Government'},
+    'AmazonReviewPolarity': {0: 'Negative polarity',
+                             1: 'Positive polarity'},
+    'AmazonReviewFull': {0: 'score 1',
+                         1: 'score 2',
+                         2: 'score 3',
+                         3: 'score 4',
+                         4: 'score 5'}
+}
+
 class ClassificationDataset:
     def __init__(self, 
         batch_size = 4,
         tokenizer = 'electra',
-        dataset = 'AG_NEWS'
+        dataset = 'YahooAnswers'
     ):
         self.batch_size = batch_size
         self.dataset_name = dataset
@@ -56,7 +106,7 @@ class ClassificationDataset:
         return entry
 
 if __name__ == '__main__':
-    data = ClassificationDataset(batch_size=64)
+    data = ClassificationDataset(batch_size=4)
 
     for _ in range(100):
         batch = data.batch()
