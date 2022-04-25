3a7f6d00ea17094bff464ffe84c0a8ad7d9c50a8
gmlwns2000 <gmlwns5176@gmail.com>
2022-04-25 15:03:08 +0900

    update tex

--- trainer/glue_base_train_batch.sh
+++ trainer/glue_base_train_batch.sh
@@ -1,9 +1,10 @@
-python -m trainer.glue_base --subset cola &&\
-python -m trainer.glue_base --subset mnli &&\
-python -m trainer.glue_base --subset mrpc &&\
-python -m trainer.glue_base --subset qnli &&\
-python -m trainer.glue_base --subset qqp &&\
-python -m trainer.glue_base --subset rte &&\
-python -m trainer.glue_base --subset sst2 &&\
-python -m trainer.glue_base --subset stsb &&\
-python -m trainer.glue_base --subset wnli
+COMMON_ARGS=""
+python -m trainer.glue_base --subset cola $COMMON_ARGS &&\
+python -m trainer.glue_base --subset mnli $COMMON_ARGS &&\
+python -m trainer.glue_base --subset mrpc $COMMON_ARGS &&\
+python -m trainer.glue_base --subset qnli $COMMON_ARGS &&\
+python -m trainer.glue_base --subset qqp  $COMMON_ARGS &&\
+python -m trainer.glue_base --subset rte  $COMMON_ARGS &&\
+python -m trainer.glue_base --subset sst2 $COMMON_ARGS &&\
+python -m trainer.glue_base --subset stsb $COMMON_ARGS &&\
+python -m trainer.glue_base --subset wnli $COMMON_ARGS 
--- trainer/glue_base_train_batch_mlai7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tmux new-session -d -s ainl1 'python -m trainer.glue_base --subset cola --device 0'
-tmux new-session -d -s ainl2 'python -m trainer.glue_base --subset mnli --device 1'
-tmux new-session -d -s ainl3 'python -m trainer.glue_base --subset mrpc --device 2'
-tmux new-session -d -s ainl4 'python -m trainer.glue_base --subset qnli --device 3'
-tmux new-session -d -s ainl5 'python -m trainer.glue_base --subset qqp  --device 4'
-tmux new-session -d -s ainl6 'python -m trainer.glue_base --subset rte  --device 5'
-tmux new-session -d -s ainl7 'python -m trainer.glue_base --subset sst2 --device 6'
-tmux new-session -d -s ainl8 'python -m trainer.glue_base --subset stsb --device 7 && python -m trainer.glue_base --subset wnli --device 7'
