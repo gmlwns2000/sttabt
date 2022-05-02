7a5fe0422511cd47e9a78f3897f1ed2a68d068cc
gmlwns2000 <gmlwns5176@gmail.com>
2022-05-03 00:22:24 +0900

    update all...?

--- trainer/glue_base_train_batch.sh
+++ trainer/glue_base_train_batch.sh
@@ -1,7 +1,7 @@
 COMMON_ARGS="--not-wiki"
-# python -m trainer.glue_base --subset cola $COMMON_ARGS &&\
-# python -m trainer.glue_base --subset mnli $COMMON_ARGS &&\
-# python -m trainer.glue_base --subset mrpc $COMMON_ARGS &&\
+python -m trainer.glue_base --subset cola $COMMON_ARGS &&\
+python -m trainer.glue_base --subset mnli $COMMON_ARGS &&\
+python -m trainer.glue_base --subset mrpc $COMMON_ARGS &&\
 python -m trainer.glue_base --subset qnli $COMMON_ARGS &&\
 python -m trainer.glue_base --subset qqp  $COMMON_ARGS &&\
 python -m trainer.glue_base --subset rte  $COMMON_ARGS &&\
