2ef07e29a94cb7a63512328fabe40f4f8717a21f
gmlwns2000 <gmlwns5176@gmail.com>
2022-07-16 21:12:14 +0900

    vit trainable

--- trainer/vit_trainer.py
+++ trainer/vit_trainer.py
@@ -1,49 +1,144 @@
+import random
 import torch
+from torch import optim, nn
 import numpy as np
+import tqdm
 import transformers
-from datasets import load_dataset
+from dataset.images_hf import ImagesHfDataset, ExamplesToBatchTransform, ViTInputTransform
 
 tasks_to_epoch = {
     'food101_5000': 10,
+    'imagenet': 10,
+}
+
+tasks_to_batch_size = {
+    'food101_5000': 16,
+    'imagenet': 16,
+}
+
+tasks_to_dataset = {
+    'food101_5000': 'food101',
+    'imagenet': 'imagenet-1k',
+}
+
+tasks_to_split = {
+    'food101_5000': 'train[:5000]',
+    'imagenet': 'train',
 }
 
 class VitTrainer:
     def __init__(self,
-        subset = 'food101_500',
-        batch_size = 4,
+        subset = 'food101_5000',
+        batch_size = -1,
         device = 0,
     ):
+        self.lr = 1e-5
+        self.weight_decay = 1e-3
+        self.amp_enable = True
+
         self.subset = subset
+        self.device = device
         self.epochs = tasks_to_epoch[self.subset]
         self.device = device
+        if batch_size <= 0:
+            batch_size = tasks_to_batch_size[self.subset]
         self.batch_size = batch_size
 
         self.init_dataloader()
-        self.reset_dataloader()
-
+        
         self.reset_train()
 
 # Initialize
 
-    def init_dataloader(self):
-        pass
+    def seed(self, seed=42):
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed) # if use multi-GPU
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
 
-    def reset_dataloader(self):
-        pass
+    def init_dataloader(self):
+        self.extractor = transformers.ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.dataset = ImagesHfDataset(
+            ExamplesToBatchTransform(ViTInputTransform(self.extractor)),
+            ExamplesToBatchTransform(ViTInputTransform(self.extractor, test=True)),
+            batch_size=self.batch_size,
+            name=tasks_to_dataset[self.subset],
+            split=tasks_to_split[self.subset],
+        )
 
     def reset_train(self):
-        self.extractor = transformers.ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
-        self.model = transformers.ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+        self.seed()
+
+        self.model = transformers.ViTForImageClassification.from_pretrained(
+            "google/vit-base-patch16-224-in21k",
+            num_labels=self.dataset.num_labels,
+            id2label=self.dataset.id2label,
+            label2id=self.dataset.label2id
+        )
+        self.model = self.model.to(self.device)
+
+        self.optimizer = self.get_optimizer(self.model)
+        self.scaler = torch.cuda.amp.GradScaler(enabled=self.amp_enable)
+    
+    def get_optimizer(self, model):
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        params = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': self.weight_decay},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+
+        kwargs = {
+            'lr':self.lr,
+            'weight_decay':self.weight_decay,
+        }
+        
+        return optim.AdamW(params, **kwargs)
 
 # Eval Impl
 
-    def eval_model(self, show_message=False):
-        pass
+    def eval_model(self, model, show_message=False):
+        model.eval()
+        from datasets import load_metric
+
+        metric = load_metric("accuracy")
+
+        pbar = tqdm.tqdm(self.dataset.get_test_iter(), desc='eval')
+        for batch in pbar:
+            batch = {k: batch[k].to(self.device, non_blocking=True) for k in batch.keys()}
+            labels = batch['labels']
+            del batch['labels']
+
+            with torch.no_grad(), torch.cuda.amp.autocast(enabled=self.amp_enable):
+                output = model(**batch)
+
+            metric.add_batch(predictions=torch.argmax(output[0], dim=-1), references=labels)
+        score = metric.compute()
+        if show_message: print(score)
+        return score
 
 # Train Impl
 
     def train_epoch(self):
-        pass
+        self.model.train()
+
+        pbar = tqdm.tqdm(self.dataset.get_train_iter())
+        for batch in pbar:
+            batch = {k: batch[k].to(self.device, non_blocking=True) for k in batch.keys()}
+            
+            with torch.cuda.amp.autocast(enabled=self.amp_enable):
+                output = self.model(**batch)
+                loss = output.loss
+            
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            self.optimizer.zero_grad()
+
+            pbar.set_description(f'[{self.epoch+1}/{self.epochs}] {loss.item():.5f}')
 
 # Main
 
@@ -51,55 +146,11 @@ class VitTrainer:
         for epoch in range(self.epochs):
             self.epoch = epoch
             self.train_epoch()
-            self.eval_model(show_message=True)
+            self.eval_model(self.model, show_message=True)
 
 def main():
-    vit_extractor = transformers.ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
-    vit_model = transformers.ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
-
-    data = load_dataset("food101", split="train[:5000]", cache_dir='./cache/datasets')
-    data = data.train_test_split(test_size=0.2)
-    labels = data["train"].features["label"].names
-    
-    from torchvision.transforms import RandomResizedCrop, Compose, RandomHorizontalFlip, Normalize, ToTensor
-    data_aug = Compose([
-        RandomResizedCrop(vit_extractor.size), 
-        RandomHorizontalFlip(), 
-        ToTensor(), 
-        Normalize(mean=vit_extractor.image_mean, std=vit_extractor.image_std)
-    ])
-    def transforms(examples):
-        examples['pixel_values'] = [data_aug(img.convert('RGB')) for img in examples['image']]
-        del examples['image']
-        return examples
-    data = data.with_transform(transforms)
-
-    data_collator = transformers.DefaultDataCollator()
-
-    training_args = transformers.TrainingArguments(
-        output_dir="./saves/vit-baseline/",
-        per_device_train_batch_size=16,
-        evaluation_strategy="steps",
-        num_train_epochs=4,
-        fp16=True,
-        save_steps=100,
-        eval_steps=100,
-        logging_steps=10,
-        learning_rate=2e-4,
-        save_total_limit=2,
-        remove_unused_columns=False,
-    )
-
-    trainer = transformers.Trainer(
-        model=vit_model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=data["train"],
-        eval_dataset=data["test"],
-        tokenizer=vit_extractor,
-    )
-    
-    trainer.train()
+    trainer = VitTrainer()
+    trainer.main()
 
 if __name__ == '__main__':
     main()
