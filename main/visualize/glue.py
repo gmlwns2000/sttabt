7245e8b784a1f070f86aa3f067b33885635ccc33
gmlwns2000 <gmlwns5176@gmail.com>
2022-09-17 02:21:15 +0900

    ready for glue vis

---
+++ main/visualize/glue.py
@@ -0,0 +1,43 @@
+def load_samples(subset):
+    pass
+
+def load_models(subset, ltp_config, concrete_config):
+    #fix following function to load if already trained.
+
+    #ltp_glue.run_exp_inner(...)
+    #concrete_glue.exp_p_logit(...)
+    pass
+
+def vis_glue(subset='sst2'):
+    configs = {
+        'sst2': {
+            #use ltp.sst2[2]
+            #use concrete.sst2[1]
+            #similar accuracy about 89%
+            'ltp': {
+                "lambda": 0.1,
+                "temperature": 0.002,
+            },
+            'concrete': {
+                "p_logit": -1.5,
+                "lr_multiplier": 1.0,
+                "epochs_multiplier": 1.0,
+                "grad_acc_multiplier": 1.0,
+            }
+        }
+    }
+    ltp_config = configs[subset]['ltp']
+    concrete_config = configs[subset]['concrete']
+
+    model_ltp, model_concrete = load_models(subset, ltp_config, concrete_config)
+    lines, batch = load_samples(subset)
+
+    #run
+
+    #visualize
+
+def main():
+    vis_glue('sst2')
+
+if __name__ == '__main__':
+    main()
