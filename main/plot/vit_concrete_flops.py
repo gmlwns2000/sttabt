6c2d383f0e44236804b65cd2a42f401b7864f961
gmlwns2000 <gmlwns5176@gmail.com>
2022-09-17 19:52:45 +0900

    left only DeiT-S variants

--- main/plot/vit_concrete_flops.py
+++ main/plot/vit_concrete_flops.py
@@ -12,21 +12,26 @@ from trainer import vit_concrete_trainer as vit_concrete
 def load_points():
     #return [flops] [accuracy] [name]
     ret = [
+        #from SPViT paper
         (1.24, 72.2, 'DeiT-T'),
+        # (1.0, 72.2, 'SPViT-DeiT-T'),
+        # (2.13, 75.00, 'T2T-ViT-10'),
+        # (0.93, 72.0, 'PS-ViT'),
+        # (0.95, 70.12, 'S$^2$ViTE'),
+
         (4.58, 79.8, 'DeiT-S'),
-        (3.25, 79.0, 'DeiT-S/320'),
-        (2.65, 78.53, 'DeiT-S/288'),
-        (2.14, 77.21, 'DeiT-S/256'),
-        (1.0, 72.2, 'SPViT-DeiT-T'),
-        (1.29, 76.87, 'SPViT-DeiT-T/256'),
         (2.64, 79.34, 'SPViT-DeiT-S'),
-        (2.00, 78.65, 'SPViT-DeiT-S/320'),
         (3.15, 79.1, 'IA-RED$^2$'),
         (3.14, 79.22, 'S$^2$ ViTE'),
         (2.4, 78.00, 'HVT-S-1'),
-        (2.13, 75.00, 'T2T-ViT-10'),
-        (0.93, 72.0, 'PS-ViT'),
-        (0.95, 70.12, 'S$^2$ViTE'),
+        
+        (3.25, 79.0, 'DeiT-S/320'),
+        # (2.00, 78.65, 'SPViT-DeiT-S/320'),
+        
+        (2.65, 78.53, 'DeiT-S/288'),
+        
+        (2.14, 77.21, 'DeiT-S/256'),
+        # (1.29, 76.87, 'SPViT-DeiT-T/256'),
     ]
     return ([ret[j][i] for j in range(len(ret))] for i in range(len(ret[0])))
 
@@ -52,9 +57,6 @@ def load_concrete(factor=4, p_logits=[-3, -2, -1.5, -1.25, -1, -0.5, 0.0, 0.5, 1
         if not os.path.exists(path_log):
             print('load_concrete: not found', path_log)
             continue
-        if not os.path.exists(path_checkpoint):
-            print('load_concrete: not found checkpoint', path_checkpoint)
-            continue
         
         #load accuracy
         with open(path_log, 'r') as f:
@@ -69,6 +71,10 @@ def load_concrete(factor=4, p_logits=[-3, -2, -1.5, -1.25, -1, -0.5, 0.0, 0.5, 1
 
         #load flops
         if not os.path.exists(path_flops):
+            if not os.path.exists(path_checkpoint):
+                print('load_concrete: not found checkpoint', path_checkpoint)
+                continue
+            
             #calc flops
             #load dataloader_test
             if dataloader_test is None:
@@ -117,6 +123,8 @@ def load_concrete(factor=4, p_logits=[-3, -2, -1.5, -1.25, -1, -0.5, 0.0, 0.5, 1
         ret.append((flops, accuracy, flops_ema, accuracy_ema))
         
     ret = sorted(ret, key=lambda it: it[0])
+    if len(ret) == 0:
+        return [], [], [], []
     return ([ret[j][i] for j in range(len(ret))] for i in range(len(ret[0])))
 
 def main():
@@ -124,6 +132,7 @@ def main():
     xs_dyvit = scale(xs_dyvit, 1e-9)
 
     xs_concrete, ys_concrete, xs_concrete_ema, ys_concrete_ema = load_concrete(factor=4)
+    xs_concrete_f8, ys_concrete_f8, xs_concrete_ema_f8, ys_concrete_ema_f8 = load_concrete(factor=8)
 
     xs_other, ys_other, labels_other = load_points()
 
@@ -139,6 +148,12 @@ def main():
         label="STTABT@f4 (Concrete) DeiT-S", color=COLOR_STTABT_CONCRETE_WITH_TRAIN,
         marker='^', linestyle='-', linewidth=1.2, zorder=99,
     )
+    if len(xs_concrete_f8) > 0:
+        plt.plot(
+            xs_concrete, ys_concrete, 
+            label="STTABT@f8 (Concrete) DeiT-S", color=COLOR_STTABT_CONCRETE_WITH_TRAIN,
+            marker='^', linestyle='-', linewidth=1.2, zorder=99,
+        )
     
     plt.scatter(xs_other, ys_other, color=COLOR_MANUAL_TOPK)
     for i, txt in enumerate(labels_other):
