da0edf44ed39194ec35d0bff65968979fb1cb522
gmlwns2000 <gmlwns5176@gmail.com>
2022-09-20 17:33:07 +0900

    flops comparision

--- main/plot/vit_concrete_flops.py
+++ main/plot/vit_concrete_flops.py
@@ -9,28 +9,34 @@ from main.plot.constants import *
 from main.visualize.vit import load_concrete as load_concrete_model
 from trainer import vit_concrete_trainer as vit_concrete
 
+COLOR_DEIT = 'blue'
+COLOR_SPVIT = 'magenta'
+COLOR_IA = 'brown'
+COLOR_S2 = 'black'
+COLOR_HVT = 'gray'
+
 def load_points():
-    #return [flops] [accuracy] [name]
+    #return [flops] [accuracy] [name] [color]
     ret = [
         #from SPViT paper
-        (1.24, 72.2, 'DeiT-T'),
+        (1.24, 72.2, 'DeiT-T', COLOR_DEIT),
         # (1.0, 72.2, 'SPViT-DeiT-T'),
         # (2.13, 75.00, 'T2T-ViT-10'),
         # (0.93, 72.0, 'PS-ViT'),
         # (0.95, 70.12, 'S$^2$ViTE'),
 
-        (4.58, 79.8, 'DeiT-S'),
-        (2.64, 79.34, 'SPViT-DeiT-S'),
-        (3.15, 79.1, 'IA-RED$^2$'),
-        (3.14, 79.22, 'S$^2$ ViTE'),
-        (2.4, 78.00, 'HVT-S-1'),
+        (4.58, 79.8, 'DeiT-S', COLOR_DEIT),
+        (2.64, 79.34, 'SPViT-DeiT-S', COLOR_SPVIT),
+        (3.15, 79.1, 'IA-RED$^2$', COLOR_IA),
+        (3.14, 79.22, 'S$^2$ ViTE', COLOR_S2),
+        (2.4, 78.00, 'HVT-S-1', COLOR_HVT),
         
-        (3.25, 79.0, 'DeiT-S/320'),
-        # (2.00, 78.65, 'SPViT-DeiT-S/320'),
+        (3.25, 79.0, 'DeiT-S/320', COLOR_DEIT),
+        # (2.00, 78.65, 'SPViT-DeiT-S/320'), # this removed since it is not originate from DeiT.
         
-        (2.65, 78.53, 'DeiT-S/288'),
+        (2.65, 78.53, 'DeiT-S/288', COLOR_DEIT),
         
-        (2.14, 77.21, 'DeiT-S/256'),
+        (2.14, 77.21, 'DeiT-S/256', COLOR_DEIT),
         # (1.29, 76.87, 'SPViT-DeiT-T/256'),
     ]
     return ([ret[j][i] for j in range(len(ret))] for i in range(len(ret[0])))
@@ -134,29 +140,29 @@ def main():
     xs_concrete, ys_concrete, xs_concrete_ema, ys_concrete_ema = load_concrete(factor=4)
     xs_concrete_f8, ys_concrete_f8, xs_concrete_ema_f8, ys_concrete_ema_f8 = load_concrete(factor=8)
 
-    xs_other, ys_other, labels_other = load_points()
+    xs_other, ys_other, labels_other, colors_other = load_points()
 
     plt.clf()
     fig, ax = plt.subplots()
     plt.plot(
         xs_dyvit, ys_dyvit, 
-        label=STR_DYNAMIC_VIT, color=COLOR_LTP_BEST_VALID,
-        marker='x', linestyle='--', linewidth=1.2, zorder=1,
+        label=STR_DYNAMIC_VIT, color='orange',
+        marker='o', linestyle='-', linewidth=1.2, zorder=1,
     )
     plt.plot(
         xs_concrete, ys_concrete, 
         label="STTABT@f4 (Concrete) DeiT-S", color=COLOR_STTABT_CONCRETE_WITH_TRAIN,
-        marker='^', linestyle='-', linewidth=1.2, zorder=99,
+        marker='^', linestyle='--', linewidth=1.2, zorder=99,
     )
     if len(xs_concrete_f8) > 0:
         plt.plot(
-            xs_concrete, ys_concrete, 
+            xs_concrete_f8, ys_concrete_f8, 
             label="STTABT@f8 (Concrete) DeiT-S", color=COLOR_STTABT_CONCRETE_WITH_TRAIN,
             marker='^', linestyle='-', linewidth=1.2, zorder=99,
         )
     
-    plt.scatter(xs_other, ys_other, color=COLOR_MANUAL_TOPK)
     for i, txt in enumerate(labels_other):
+        plt.scatter(xs_other[i], ys_other[i], color=colors_other[i])
         ax.annotate(txt, (xs_other[i], ys_other[i]), fontsize=6)
     
     plt.legend(prop={'size': 9})
@@ -165,7 +171,7 @@ def main():
     plt.ylabel(STR_TOP1_ACCURACY)
 
     y_bot, y_top = plt.ylim()
-    y_bot = y_top - (y_top-y_bot) * 0.6
+    y_bot = y_top - (y_top-y_bot) * 0.96
     plt.ylim(y_bot, y_top)
     
     plt.grid(which='both', axis='both')
